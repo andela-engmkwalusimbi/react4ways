09735cba5ed05ba5f9b4cb71adbb09ef7077801c
Mahad Walusimbi <engmk123@gmail.com>
2017-12-20 14:31:22 +0300

    Add thunks

---
+++ src/configureStore.js
@@ -0,0 +1,7 @@
+import { createStore, applyMiddleware } from 'redux'
+import app from './reducers'
+import thunk from 'redux-thunk'
+
+export default function configureStore() {
+  return createStore(app, applyMiddleware(thunk))
+}
