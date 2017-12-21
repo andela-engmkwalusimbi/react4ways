078290c8f59f025b1b5d5760167419e9c7d187f0
Mahad Walusimbi <engmk123@gmail.com>
2017-12-22 01:11:04 +0300

    Add redux observables

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,22 @@
-import { FETCHING_DATA } from './constants';
+import { FETCHING_DATA, FETCHING_DATA_SUCCESS, FETCHING_DATA_FAILURE } from './constants';
 
 
 export const fetchData = () => {
     return {
         type: FETCHING_DATA
+    }
+}
+
+export function getDataSuccess (data) {
+    return {
+      type: FETCHING_DATA_SUCCESS,
+      data
+    }
+}
+
+export function getDataFailure (error) {
+    return {
+      type: FETCHING_DATA_FAILURE,
+      errorMessage: error
     }
 }
